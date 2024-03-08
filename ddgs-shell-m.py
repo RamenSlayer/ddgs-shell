fa0d267fd3e772af4b8815906d5ce7b0b0729e27
Daud <mike.dizhur@gmail.com>
2024-03-08 18:04:54 -0500

    Created mobile version DDGS from original doesn't work on android. Maybe less functionality? Which is why there are 2 versions now

---
+++ ddgs-shell-m.py
@@ -0,0 +1,237 @@
+#!/usr/bin/python3
+
+from ddg import Duckduckgo
+# from argparse import ArgumentParser as argparser
+from rich.console import Console
+from rich.markup import escape
+
+width = 100
+console = Console(width=width)
+print = console.print
+
+api = Duckduckgo()
+
+shell_message = """
+q\t\tquit
+v\t\tprint out the results
+t TEXT\t\ttext to search for
+e TEXT\t\texclude words, no text unsets it
+el\t\tset/unset excluding words from links, off by default
+ec\t\tset/unset case sensitivity for word exclusion
+p\t\tprint all variables
+s\t\texecutes search
+m NUM\t\tset max number of search results
+d\t\tset/unset debugging info, off by default
+vs\t\tset/unset stepping for printing results, off by default
+o [FILES]\t\toutputs results to file(s)
+w WIDTH\t\tto set console to certain width
+h\t\tprint this message
+"""
+
+
+def Exclude(results: list,
+            words: list,
+            ex_links: bool = False,
+            ignore_case: bool = True,
+            preserve_initial: bool = True):
+    if preserve_initial:
+        results = results.copy()
+    end = len(results)
+    i = 0
+    while i < end:
+        title = results[i]["title"]
+        body = results[i]["description"]
+        link = results[i]["url"]
+        if ignore_case:
+            body = body.lower()
+            title = title.lower()
+            link = link.lower()
+        for word in words:
+            if ignore_case:
+                word = word.lower()
+            if word in title:
+                results.__delitem__(i)
+                end -= 1
+                i -= 1
+                break
+            elif word in body:
+                results.__delitem__(i)
+                end -= 1
+                i -= 1
+                break
+            elif ex_links and word in link:
+                results.__delitem__(i)
+                end -= 1
+                i -= 1
+                break
+        i += 1
+    return results
+
+
+def Print_Search(results: list, step: bool = False):
+    if step:
+        print("[yellow]q to stop scrolling if needed[/yellow]")
+    for res in results:
+        print("[b]"+escape(res["title"])+"[/b]")
+        print(escape(res["url"]))
+        print(escape(res["description"]))
+        print("-----------")
+        if step:
+            tmp = input("Next? ")
+            if tmp == 'q':
+                break
+    return 0
+
+
+def Output_Search(results):
+    out = []
+    for res in results:
+        out.append("\n".join((res["title"], res["url"], res["description"])))
+    return "\n".join(out)
+
+
+def Shell(*args):
+    print("Welcome to DDGS expanded search")
+    print(shell_message)
+    text = ""
+    ex_words = None
+    results = None
+    debug = False
+    ex_links = False
+    ignore_case = True
+    step = False
+    max_res = None
+    while True:
+        inp = input(" > ")
+        if inp == 'q':
+            print("Exiting")
+            break
+        else:
+            inp = inp.split()
+        match inp[0]:
+            case "t":
+                text = " ".join(inp[1:])
+                print(f"[green]Search text: {text}[green]")
+            case "e":
+                if len(inp) == 1:
+                    ex_words = None
+                    print("[green]Not excluding any words[/green]")
+                    continue
+                ex_words = inp[1:]
+                print(f"[green]Excluding selected words: {ex_words}[/green]")
+            case "el":
+                ex_links = not ex_links
+                print(f"[green]Exluding text in links: {ex_links}[/green]")
+            case "ec":
+                ignore_case = not ignore_case
+                print(f"[green]Ignoring exclusion case: {ignore_case}[/green]")
+            case "m":
+                if len(inp) > 2:
+                    print("[red]max requires 1 or 0 arguments[/red]")
+                    continue
+                if len(inp) == 1:
+                    max_res = None
+                    print("[green]max set to None[/green]")
+                try:
+                    max_res = int(inp[1])
+                    print(f"[green]Max results set to {max_res}[/green]")
+                except Exception as e:
+                    print(f"[red]Couldn't set max results to {inp[1]}[red]")
+                    if debug:
+                        print(f"[red]{e}[/red]")
+                    continue
+            case "p":
+                print(f"Search text: {text}",
+                      f"Max results: {max_res}",
+                      f"Excluded words: {ex_words}",
+                      f"Stepping: {step}",
+                      f"Debugging: {debug}",
+                      f"Excluding links: {ex_links}",
+                      f"Ignoring case: {ignore_case}",
+                      f"Width: {console.width}",
+                      sep="\n")
+                if results is None:
+                    print(f"Number of results: {results}")
+                else:
+                    print(f"Number of results: {len(results)}")
+            case "s":
+                if text == "":
+                    print("[red]No search text![/red]")
+                    continue
+                results = api.search(text)
+                if not results['success']:
+                    print("[red]Couldn't get results[/red]")
+                    results = None
+                    continue
+                results = results["data"]
+                print("[green]Results acquired[/green]")
+            case "v":
+                if results is None:
+                    print("[red]No results![red]")
+                    continue
+                tmp_results = results
+                if max_res is None:
+                    pass
+                else:
+                    tmp_results = results[:max_res]
+                if ex_words is not None:
+                    Print_Search(Exclude(tmp_results,
+                                         ex_words,
+                                         ex_links,
+                                         ignore_case),
+                                 step=step)
+                    del tmp_results
+                    continue
+                Print_Search(tmp_results, step=step)
+                del tmp_results
+                continue
+            case "vs":
+                step = not step
+                print(f"[green]Stepping set to: {step}[/green]")
+            case "d":
+                debug = not debug
+                print(f"[green]Debugging set to: {debug}[/green]")
+            case "h":
+                print(shell_message)
+            case "w":
+                if len(inp) != 2:
+                    print("[red]!w takes exactly 1 argument[/red]")
+                    continue
+                try:
+                    console.width = int(inp[1])
+                    print(f"[green]Width set to {console.width}[/green]")
+                except Exception as e:
+                    print(f"[red]Couldn't set width to {inp[1]}[/red]")
+                    if debug:
+                        print("[red]" + escape(e) + "[/red]")
+            case "o":
+                if results is None:
+                    print("[red]No results to write[/red]")
+                if len(inp) < 2:
+                    print("[red]No file specified[/red]")
+                    continue
+                for dest in inp[1:]:
+                    try:
+                        with open(dest, 'w') as f:
+                            if ex_words is not None:
+                                f.write(Output_Search(Exclude(results,
+                                                              ex_words,
+                                                              ex_links,
+                                                              ignore_case)))
+                                print(f"[green]Wrote output to: {dest}[/green]")
+                                continue
+                            f.write(Output_Search(results))
+                        print(f"[green]Wrote output to: {dest}[/green]")
+                    except Exception as e:
+                        print(f"[red]Couldn't write to: {dest}[/red]")
+                        if debug:
+                            print(f"[red]{e}[/red]")
+    return 0
+
+
+def main() -> int:
+    Shell()
+
+
+if __name__ == "__main__":
+    main()
